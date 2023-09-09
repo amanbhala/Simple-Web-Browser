06c8865d5c157c0f9da88e2d0b5510fd67e76dbf
Aman Bhala <amanbhala@amans-air.mynetworksettings.com>
2023-09-09 13:53:34 -0700

    Created simple web browser and some services

---
+++ WebBrowser/Form1.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace WebBrowser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.TextBox1 = new System.Windows.Forms.TextBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button23 = new System.Windows.Forms.Button();
+            this.button24 = new System.Windows.Forms.Button();
+            this.button25 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox15 = new System.Windows.Forms.TextBox();
+            this.button9 = new System.Windows.Forms.Button();
+            this.textBox16 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 16);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 50);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Back";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(112, 16);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 49);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Next";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(193, 15);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(107, 50);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Refresh";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(607, 15);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(140, 50);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Search";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(763, 15);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(126, 50);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Home";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // TextBox1
+            // 
+            this.TextBox1.Location = new System.Drawing.Point(315, 27);
+            this.TextBox1.Name = "TextBox1";
+            this.TextBox1.Size = new System.Drawing.Size(273, 26);
+            this.TextBox1.TabIndex = 8;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(26, 79);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(863, 434);
+            this.webBrowser1.TabIndex = 9;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(266, 607);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(157, 26);
+            this.textBox3.TabIndex = 29;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(266, 639);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(157, 26);
+            this.textBox4.TabIndex = 30;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(28, 686);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(72, 36);
+            this.button8.TabIndex = 31;
+            this.button8.Text = "Add";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click_1);
+            // 
+            // button23
+            // 
+            this.button23.Location = new System.Drawing.Point(351, 686);
+            this.button23.Name = "button23";
+            this.button23.Size = new System.Drawing.Size(72, 36);
+            this.button23.TabIndex = 32;
+            this.button23.Text = "Divide";
+            this.button23.UseVisualStyleBackColor = true;
+            this.button23.Click += new System.EventHandler(this.button23_Click);
+            // 
+            // button24
+            // 
+            this.button24.Location = new System.Drawing.Point(242, 686);
+            this.button24.Name = "button24";
+            this.button24.Size = new System.Drawing.Size(90, 36);
+            this.button24.TabIndex = 33;
+            this.button24.Text = "Multiply";
+            this.button24.UseVisualStyleBackColor = true;
+            this.button24.Click += new System.EventHandler(this.button24_Click);
+            // 
+            // button25
+            // 
+            this.button25.Location = new System.Drawing.Point(121, 686);
+            this.button25.Name = "button25";
+            this.button25.Size = new System.Drawing.Size(93, 36);
+            this.button25.TabIndex = 34;
+            this.button25.Text = "Subtract";
+            this.button25.UseVisualStyleBackColor = true;
+            this.button25.Click += new System.EventHandler(this.button25_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(251, 747);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(172, 26);
+            this.textBox5.TabIndex = 36;
+            // 
+            // textBox15
+            // 
+            this.textBox15.Location = new System.Drawing.Point(829, 747);
+            this.textBox15.Name = "textBox15";
+            this.textBox15.Size = new System.Drawing.Size(191, 26);
+            this.textBox15.TabIndex = 49;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(668, 686);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(197, 36);
+            this.button9.TabIndex = 50;
+            this.button9.Text = "Calculate Price";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // textBox16
+            // 
+            this.textBox16.Location = new System.Drawing.Point(829, 639);
+            this.textBox16.Name = "textBox16";
+            this.textBox16.Size = new System.Drawing.Size(191, 26);
+            this.textBox16.TabIndex = 51;
+            this.textBox16.TextChanged += new System.EventHandler(this.textBox16_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 607);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 20);
+            this.label1.TabIndex = 57;
+            this.label1.Text = "Enter First Number";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 639);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(167, 20);
+            this.label2.TabIndex = 58;
+            this.label2.Text = "Enter Second Number";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 753);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 20);
+            this.label3.TabIndex = 59;
+            this.label3.Text = "Output";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(546, 645);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(139, 20);
+            this.label4.TabIndex = 60;
+            this.label4.Text = "Enter Stock Name";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(596, 753);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 20);
+            this.label5.TabIndex = 61;
+            this.label5.Text = "Stock Price";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(108, 555);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(281, 20);
+            this.label6.TabIndex = 62;
+            this.label6.Text = "BASIC CALCULATOR APPLICATION ";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(635, 555);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(223, 20);
+            this.label7.TabIndex = 63;
+            this.label7.Text = "STOCK PRICE APPLICATION";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1116, 815);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox16);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.textBox15);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button25);
+            this.Controls.Add(this.button24);
+            this.Controls.Add(this.button23);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.TextBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox TextBox1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button23;
+        private System.Windows.Forms.Button button24;
+        private System.Windows.Forms.Button button25;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox15;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.TextBox textBox16;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ WebBrowser/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebBrowser
+{
+    public partial class Form1 : Form
+    {
+        // Home page URL
+        private string homepageUrl = "https://www.google.com";
+        public double result = 0.0;
+        public double number1 = 0.0;
+        public double number2 = 0.0;
+        public Form1()
+        {
+            InitializeComponent();
+            webBrowser1.ScriptErrorsSuppressed = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // If there is a possibility of going backward then go backward
+            if(webBrowser1.CanGoBack)
+            { 
+                webBrowser1.GoBack(); 
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // If there is a possibility of going forward, then go forward
+            if (webBrowser1.CanGoForward)
+            {
+                webBrowser1.GoForward();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Refresh the webpage
+            webBrowser1.Refresh();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string userInput = TextBox1.Text.Trim();
+            string url = "";
+            // Checking that user input is not empty
+            if (!string.IsNullOrEmpty(userInput))
+            {
+                // If user input ends with .com , we will create the url
+                if (userInput.EndsWith(".com"))
+                {
+                    if (!userInput.StartsWith("http://") && !userInput.StartsWith("https://"))
+                    {
+                        url = "https://" + userInput;
+                    }
+                    webBrowser1.Navigate(userInput);
+                }
+            }
+            else
+            {
+                // If user input does not end in .com , we create a search engine(Google) URL with the query parameter
+                string searchEngineUrl = "https://www.google.com/search?q=" + Uri.EscapeDataString(userInput);
+                webBrowser1.Navigate(searchEngineUrl);
+            }
+        }
+        
+        private void button5_Click(object sender, EventArgs e)
+        {
+            // Navigate to the home page
+            webBrowser1.Navigate(homepageUrl);
+        }
+
+        private void webBrowser2_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void Calculate_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button8_Click_1(object sender, EventArgs e)
+        {
+            result = number1 + number2;
+            textBox5.Text = result.ToString();
+        }
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            // Getting first number from user
+            try
+            {
+                number1 = Double.Parse(textBox3.Text);
+            }
+            catch(FormatException)
+            {
+                MessageBox.Show("Invalid input.", "Please enter a float value.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            // Getting second number from user
+            try
+            {
+                number2 = Double.Parse(textBox4.Text);
+            }
+            catch(FormatException)
+            {
+                MessageBox.Show("Invalid input.", "Please enter a float value.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button25_Click(object sender, EventArgs e)
+        {
+            result = number1 - number2;
+            textBox5.Text = result.ToString();
+        }
+
+        private void button24_Click(object sender, EventArgs e)
+        {
+            result = number1 * number2;
+            textBox5.Text = result.ToString();
+        }
+
+        private void button23_Click(object sender, EventArgs e)
+        {
+            result = number1 / number2;
+            textBox5.Text = result.ToString();
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox11_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            // Calling the stock service API and putting the value in the result textbox
+            try
+            {
+                StockService.ServiceClient serviceClient = new StockService.ServiceClient();
+                textBox15.Text = serviceClient.getStockquote(textBox16.Text).ToString();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Invalid input.", "Please enter correct stock code.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void textBox16_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox11_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
